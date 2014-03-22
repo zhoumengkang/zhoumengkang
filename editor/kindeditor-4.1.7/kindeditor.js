21965a140a564e08821cb2871f2986d59514ba63
zhoumengkang <i@zhoumengkang.com>
2014-03-22 22:48:35 +0800

    修改编辑器里面默认的字体的大小

--- editor/kindeditor-4.1.7/kindeditor.js
+++ editor/kindeditor-4.1.7/kindeditor.js
@@ -3507,7 +3507,7 @@ function _getInitHtml(themesPath, bodyClass, cssPath, cssData) {
 		'<style>',
 		'html {margin:0;padding:0;}',
 		'body {margin:0;padding:5px;}',
-		'body, td {font:12px/1.5 "sans serif",tahoma,verdana,helvetica;}',
+		'body, td {font:18px/1.5 "sans serif",tahoma,verdana,helvetica;}',
 		'body, p, div {word-wrap: break-word;}',
 		'p {margin:5px 0;}',
 		'table {border-collapse:collapse;}',
