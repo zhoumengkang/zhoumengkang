3d833e26dabccbe71959b49de67bcc4f4699f28d
zhoumengkang <i@zhoumengkang.com>
2014-07-25 18:13:17 +0800

    修改下标签数目

--- view/search.php
+++ view/search.php
@@ -4,7 +4,7 @@ include 'header.php';
     <div id="wrap">
         <div class="main" style="margin-bottom: 15px;">
             <span class="side_title" style=" float: left; margin-right: 20px; ">搜索</span>
-            <form name="search" action="<?php echo U('Blog/search');?>" method="get">
+            <form name="search" action="<?php echo U('Blog/search');?>" method="post" data-test="1">
                 <input type="text" name="keyword" value="" placeholder="输入关键词" style="height: 20px;padding: 3px 5px;font-size: 14px;" />
             </form>
         </div>
