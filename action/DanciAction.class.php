ac896d8d3d897d2170a9484daa45c57b3be6ce53
zhoumengkang <i@zhoumengkang.com>
2014-07-26 23:28:39 +0800

    把引号转为实体

--- action/DanciAction.class.php
+++ action/DanciAction.class.php
@@ -24,7 +24,7 @@ class DanciAction extends Action{
                 $_POST=null;
             }else{
                 //insert
-                $sql = "insert into z_danciben(`word`,`translate`,`sentence`,`from`)values('{$_POST['word']}','{$_POST['translate']}','{$_POST['sentence']}','{$_POST['from']}')";
+                $sql = "insert into z_danciben(`word`,`translate`,`sentence`,`from`)values('{$_POST['word']}','{$_POST['translate']}','".htmlspecialchars($_POST['sentence'])."','".htmlspecialchars($_POST['from'])."')";
                 $flag = d()->q($sql);
                 $_POST=null;
             }
