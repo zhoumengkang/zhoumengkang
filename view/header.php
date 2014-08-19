2db9997848ce032daf8fa505489d8158e4497b1b
zhoumengkang <i@zhoumengkang.com>
2014-08-19 10:44:25 +0800

    修改header头

--- view/header.php
+++ view/header.php
@@ -33,7 +33,6 @@ if(defined('ROUTE') && ROUTE){
 <head lang="zh-cn">
     <meta charset="utf-8">
 	<title><?php echo $this->title ;?></title>
-    <meta property="qc:admins" content="41110667075556731676375" />
     <?php if($_GET['a'] == 'blog'){?><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"><?php }?>
 	<meta name="description" content="<?php if($this->description){echo $this->description;}else{ echo '周梦康的博客，记录着我的学习笔记，也记录着我的生活琐事。';}?>"/>
     <meta name="keywords" content="<?php if($this->keywords){echo $this->keywords;}else{ echo '周梦康,梦康,mengkang,北剅轩';}?>" />
